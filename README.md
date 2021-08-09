4ee55d2a4585a9a432f4717facf45e5f08a691b0
myh2910 <minleeyohan@gmail.com>
2021-08-09 06:52:33 -0500

    deleted unnecessary files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Manim
-Python source codes written for [Raul Alcantara's project](https://education-initiative-test.herokuapp.com). Visit [my youtube channel](https://www.youtube.com/channel/UCubDjsMsD8vUNJL7wAqRASQ) to watch the videos.
+Python source codes written for [Raul Alcantara's project](https://www.youtube.com/channel/UCqVBiz4qJFCwmYvAkMUn_0A).
 
 ## Installation
 - `sudo pacman -Syu`
@@ -11,5 +11,5 @@ Python source codes written for [Raul Alcantara's project](https://education-ini
 
 For further information, visit [Manim Community's website](https://docs.manim.community).
 
-## Running
+## Run
 First, clone this repository and run `python render` at its respective directory.
--- render/__main__.py
+++ render/__main__.py
@@ -3,17 +3,6 @@ import time
 from datetime import datetime
 from pydub import AudioSegment
 
-def thumbnail(year, number, quality='k', extra=None, scene='Thumbnail'):
-	year, number = str(year), str(number)
-	command = 'python -m manim'
-	if extra:
-		command += f' {extra}'
-	command += f' -q{quality} -o {scene} render/t{year}_{number}.py {scene}'
-	start = time.time()
-	os.system(command)
-	end = time.time()
-	print(f'{end - start} seconds elapsed.')
-
 def video(year, number, quality='k', extra=None, scene='Video'):
 	year, number = str(year), str(number)
 	title = f'IMO_{year}_problema_{number}_yohan_min'
@@ -31,9 +20,3 @@ def format_convert(from_dir, to_dir=None, from_format='flac', to_format='mp3'):
 		to_dir = 'assets/{}.mp3'.format(datetime.now().strftime('%Y%m%d_%H%M%S_%f')[:-4])
 	audio = AudioSegment.from_file(from_dir, format=from_format)
 	audio.export(to_dir, format=to_format)
-
-#for args in [(2015, 4), (2018, 1), (2020, 1), (2020, 3), (2021, 1)]:
-	#thumbnail(*args)
-	#video(*args)
-#thumbnail(2020, 3)
-video(2020, 3)
